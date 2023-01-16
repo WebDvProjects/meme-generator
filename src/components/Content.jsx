fd47b734db9112c84e632a4b60f3be82cbace48a
Cassian <kahemacassian@gmail.com>
2023-01-16 21:59:40 +1030

    Add text to image

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
-import memes from "../meme-data";
+import memesData from "../meme-data";
 
 function Form(props) {
   return (
     <div className="form">
-      <input className="top statement" type="text" placeholder="Top Text" />
+      <input
+        className="top statement"
+        type="text"
+        name="topText"
+        value={props.topText}
+        placeholder="Top Text"
+        onChange={props.handleInputChange}
+      />
       <input
         className="bottom statement"
         type="text"
+        name="bottomText"
+        value={props.bottomText}
         placeholder="Bottom Text"
+        onChange={props.handleInputChange}
       />
       <button type="button" className="form--btn" onClick={props.handleClick}>
         Create a new meme Image 🖼
@@ -19,29 +29,48 @@ function Form(props) {
 
 function Meme(props) {
   return (
-    <div className="meme" data-has-meme={!!props.meme}>
+    <div className="meme" data-has-meme={!!props.url}>
       {/* render img only if meme object is valid */}
-      {!!props.meme && (
-        <img className="meme--img" src={props.meme.url} alt="meme" />
-      )}
-      <h2 className="meme--text top">Top text</h2>
-      <h2 className="meme--text bottom">Bottom text</h2>
+      {!!props.url && <img className="meme--img" src={props.url} alt="meme" />}
+      <h2 className="meme--text top">{props.topText}</h2>
+      <h2 className="meme--text bottom">{props.bottomText}</h2>
     </div>
   );
 }
 
 export default function Content() {
-  const [meme, setMeme] = useState(null);
+  const [allMemes, setAllMemes] = useState(memesData);
+  const [meme, setMeme] = useState({
+    // default object state
+    topText: "Top text",
+    bottomText: "Bottom text",
+    url: "https://i.imgflip.com/1bij.jpg",
+  });
 
-  function handleClick() {
-    const randIndx = Math.floor(Math.random() * memes.length);
-    setMeme(memes[randIndx]);
+  function getMeme() {
+    const randIndx = Math.floor(Math.random() * allMemes.length);
+    const randomMemeUrl = allMemes[randIndx].url;
+    setMeme((meme) => ({ ...meme, url: randomMemeUrl }));
+    console.log(meme);
+  }
+
+  function handleInputChange(e) {
+    const { name, value, type } = e.target;
+    setMeme((prevMemeInfo) => ({
+      ...prevMemeInfo,
+      [name]: value,
+    }));
   }
 
   return (
     <main className="content">
-      <Form handleClick={handleClick} />
-      <Meme meme={meme} />
+      <Form
+        topText={meme.topText}
+        bottomText={meme.bottomText}
+        handleClick={getMeme}
+        handleInputChange={handleInputChange}
+      />
+      <Meme {...meme} />
     </main>
   );
 }
