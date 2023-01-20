5c19ee57e0e768c517ac1718a99230d08896749c
caykay <kahemacassian@gmail.com>
2023-01-20 18:45:22 +1030

    API fetch and remove strict mode: - Strict mode caused double rendering of the components which would then   trigger the API effects twice. This only happens in dev according to   react.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import memesData from "../meme-data";
 import Form from "./Form";
 import Meme from "./Meme";
 
 export default function Content() {
-  const [allMemes, setAllMemes] = useState(memesData);
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => {
+        const { memes } = data.data;
+        setAllMemes(() => memes);
+        console.log("Memes loaded");
+      });
+  }, []);
+
   const [meme, setMeme] = useState({
     // default object state
     topText: "Top text",
     bottomText: "Bottom text",
-    url: "https://i.imgflip.com/1bij.jpg",
+    url: "https://i.imgflip.com/1ur9b0.jpg",
   });
 
   function getMeme() {
+    if (!allMemes) return;
     const randIndx = Math.floor(Math.random() * allMemes.length);
     const randomMemeUrl = allMemes[randIndx].url;
     setMeme((meme) => ({ ...meme, url: randomMemeUrl }));
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,39 @@
+import { Skeleton } from "@mui/material";
+
 export default function Meme(props) {
   return (
-    <div className="meme" data-has-meme={!!props.url}>
-      {/* render img only if meme object is valid */}
-      {!!props.url && <img className="meme--img" src={props.url} alt="meme" />}
-      <h2 className="meme--text top">{props.topText}</h2>
-      <h2 className="meme--text bottom">{props.bottomText}</h2>
-    </div>
+    /* render img only if meme object is valid */
+    !!props.url ? (
+      <div className="meme">
+        <img className="meme--img" src={props.url} alt="meme" />
+        <h2 className="meme--text top">{props.topText}</h2>
+        <h2 className="meme--text bottom">{props.bottomText}</h2>
+      </div>
+    ) : (
+      <Skeleton
+        variant="rectangular"
+        animation="wave"
+        sx={{
+          height: "400px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: "1rem",
+        }}>
+        <div
+          style={{
+            fontSize: "1.5rem",
+            fontWeight: "500",
+            background: "linear-gradient(90deg, pink, var(--theme-color))",
+            backgroundClip: "text",
+            WebkitBackgroundClip: "text",
+            color: "transparent",
+            visibility: "visible",
+          }}
+          key={"no-meme"}>
+          No Meme
+        </div>
+      </Skeleton>
+    )
   );
 }
